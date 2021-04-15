ae63197331609a083a3794afed570096306137d5
Elisa Camillo <elisatcamillo@gmail.com>
2021-04-15 20:45:10 -0300

    Initial Commit

---
+++ DetailsLibrary/DetailsLibrary.h
@@ -0,0 +1,18 @@
+//
+//  DetailsLibrary.h
+//  DetailsLibrary
+//
+//  Created by Elisa Camillo on 15/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DetailsLibrary.
+FOUNDATION_EXPORT double DetailsLibraryVersionNumber;
+
+//! Project version string for DetailsLibrary.
+FOUNDATION_EXPORT const unsigned char DetailsLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DetailsLibrary/PublicHeader.h>
+
+
