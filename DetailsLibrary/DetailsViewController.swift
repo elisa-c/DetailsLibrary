6f0b62ffb9922215df3c2b829b1458064f2c0117
Elisa Camillo <elisatcamillo@gmail.com>
2021-04-15 21:44:08 -0300

    construção inicial do storyboard

---
+++ DetailsLibrary/DetailsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  DetailsViewController.swift
+//  DetailsLibrary
+//
+//  Created by Elisa Camillo on 15/04/21.
+//
+
+import UIKit
+
+public class DetailsViewController: UIViewController {
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
