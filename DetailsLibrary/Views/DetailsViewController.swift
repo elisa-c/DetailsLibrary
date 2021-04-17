2013b38323c85f4bfb13776be49d236e25dd0e5a
Elisa Camillo <elisatcamillo@gmail.com>
2021-04-16 21:39:04 -0300

    adicionando lógica para inicializar no projeto principal

--- DetailsLibrary/Views/DetailsViewController.swift
+++ DetailsLibrary/Views/DetailsViewController.swift
@@ -17,16 +17,41 @@ public class DetailsViewController: UIViewController {
     @IBOutlet weak var lastHour: UILabel!
     @IBOutlet weak var lastDay: UILabel!
     @IBOutlet weak var lastMonth: UILabel!
-    
-        
+
+
     public override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // adiciona borda à view que contém o botão
         borderButton.layer.borderWidth = 10
         borderButton.layer.borderColor = UIColor.white.cgColor
 
     }
-    
 
+
+
+}
+
+
+extension UIViewController {
+    public static var bundleUI: Bundle {
+        var bundle: Bundle
+        if let bundeLet = Bundle(identifier: "com.br.everis.DetailsLibrary") {
+            bundle = bundeLet
+        } else {
+            bundle = Bundle(for: self)
+        }
+        return bundle
+    }
+    public class func fromSB() -> Self {
+        return fromSB(viewType: self)
+    }
+    public class func fromSB<T: UIViewController>(viewType: T.Type) -> T {
+        let sb = UIStoryboard(name: "Details", bundle: bundleUI)
+        if let vc = sb.instantiateViewController(withIdentifier: "DetailsID") as? T {
+            vc.loadViewIfNeeded()
+            return vc
+        }
+        return T()
+    }
 }
